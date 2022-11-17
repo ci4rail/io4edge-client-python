afbf1b4e84d7d6723413a70b59890c8650b117dd
popples <klauspopp@gmx.de>
2022-11-17 20:50:53 +0000

    WIP

--- io4edge_client/base/client.py
+++ io4edge_client/base/client.py
@@ -1,8 +1,10 @@
 from zeroconf import Zeroconf
 from .socket_transport import SocketTransport
 
+
 class Client:
     def __init__(self, addr: str):
+        # self._transport = SocketTransport(ip, port)
         pass
 
     def write_msg(self, data: bytes) -> int:
@@ -10,3 +12,20 @@ class Client:
 
     def read_msg(self) -> bytes:
         pass
+
+    @staticmethod
+    def _net_address_split(addr: str):
+        # split string "ip:port" into tuple (ip, port)
+        fields = addr.split(":")
+        if len(fields) != 2:
+            raise ValueError("invalid address")
+        return fields[0], int(fields[1])
+
+    @staticmethod
+    def _split_service(service: str):
+        fields = service.split(".")
+        if len(fields) < 3:
+            raise ValueError("service address not parseable (one of these are missing: instance, service, protocol)")
+        service = fields[-2] + "." + fields[-1]
+        instance = ".".join(fields[:-2])
+        return instance, service
--- io4edge_client/base/socket_transport.py
+++ io4edge_client/base/socket_transport.py
@@ -1,29 +1,33 @@
 import socket
 import struct
 
+
 class SocketTransport:
     def __init__(self, host, port):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((host, port))
         self._socket = s
 
-    def write_msg(self, data: bytes ) -> int:
-        hdr = struct.pack('<HL', 0xEDFE, len(data))
-        return self._socket.sendall(hdr+data)
+    def write_msg(self, data: bytes) -> int:
+        hdr = struct.pack("<HL", 0xEDFE, len(data))
+        return self._socket.sendall(hdr + data)
 
     def read_msg(self) -> bytes:
-        hdr = self.rcv_all(6)
-        if hdr[0:2] == b'\xfe\xed':
-            len = struct.unpack('<L', hdr[2:6])[0]
-            data = self.rcv_all(len)
+        hdr = self._rcv_all(6)
+        if hdr[0:2] == b"\xfe\xed":
+            len = struct.unpack("<L", hdr[2:6])[0]
+            data = self._rcv_all(len)
             return data
-        raise RuntimeError('bad magic')
+        raise RuntimeError("bad magic")
 
-    def _rcv_all(self, n: int) -> bytes:
-        remaining = n
+    def _rcv_all(self, data_len: int) -> bytes:
+        remaining = data_len
         buf = bytearray()
         while remaining > 0:
             data = self._socket.recv(remaining)
             buf.extend(data)
             remaining -= len(data)
         return buf
+
+    def close(self):
+        self._socket.close()
