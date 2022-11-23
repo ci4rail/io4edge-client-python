0cba57287df2996ed445afdb4fe1b04a0d3983d2
Klaus Popp <klauspopp@gmx.de>
2022-11-23 20:16:59 +0000

    add docs to functionblock and mvbsniffer

--- io4edge_client/functionblock/client.py
+++ io4edge_client/functionblock/client.py
@@ -8,6 +8,12 @@ import google.protobuf.any_pb2 as AnyPb
 
 
 class Client:
+    """
+    io4edge functionblock client.
+    @param addr: address of io4edge function block (mdns name or "ip:port" address)
+    @param command_timeout: timeout for commands in seconds
+    """
+
     def __init__(self, addr: str, command_timeout=5):
         self._client = BaseClient(addr)
         self._stream_queue_mutex = (
@@ -27,15 +33,27 @@ class Client:
         self._read_thread_id.start()
 
     def upload_configuration(self, fs_cmd):
+        """
+        Upload configuration to io4edge function block.
+        @param fs_cmd: protobuf message with the function specific configuration
+        @raises RuntimeError: if the command fails
+        @raises TimeoutError: if the command times out
+        """
         fs_any = AnyPb.Any()
         fs_any.Pack(fs_cmd)
 
         fb_cmd = FbPb.Command()
         fb_cmd.Configuration.functionSpecificConfigurationSet.CopyFrom(fs_any)
-        fb_res = self._command(fb_cmd)
-        return fb_res.Configuration.functionSpecificConfigurationSet
+        self._command(fb_cmd)
 
     def download_configuration(self, fs_cmd, fs_response):
+        """
+        Download configuration from io4edge function block.
+        @param fs_cmd: protobuf message with the function specific configuration (mostly empty)
+        @param fs_response: protobuf message that is filled with the function specific configuration response
+        @raises RuntimeError: if the command fails
+        @raises TimeoutError: if the command times out
+        """
         fs_any = AnyPb.Any()
         fs_any.Pack(fs_cmd)
 
@@ -47,6 +65,13 @@ class Client:
         )
 
     def describe(self, fs_cmd, fs_response):
+        """
+        Describe the function block (call the firmware describe function).
+        @param fs_cmd: protobuf message with the function specific describe request (mostly empty)
+        @param fs_response: protobuf message that is filled with the function specific describe response
+        @raises RuntimeError: if the command fails
+        @raises TimeoutError: if the command times out
+        """
         fs_any = AnyPb.Any()
         fs_any.Pack(fs_cmd)
 
@@ -58,6 +83,13 @@ class Client:
         )
 
     def function_control_set(self, fs_cmd, fs_response):
+        """
+        Execute "function control set" command on io4edge function block.
+        @param fs_cmd: protobuf message with the function specific function control set request
+        @param fs_response: protobuf message that is filled with the function specific function control set response
+        @raises RuntimeError: if the command fails
+        @raises TimeoutError: if the command times out
+        """
         fs_any = AnyPb.Any()
         fs_any.Pack(fs_cmd)
 
@@ -69,6 +101,13 @@ class Client:
         )
 
     def function_control_get(self, fs_cmd, fs_response):
+        """
+        Execute "function control get" command on io4edge function block.
+        @param fs_cmd: protobuf message with the function specific function control get request (mostly empty)
+        @param fs_response: protobuf message that is filled with the function specific function control get response
+        @raises RuntimeError: if the command fails
+        @raises TimeoutError: if the command times out
+        """
         fs_any = AnyPb.Any()
         fs_any.Pack(fs_cmd)
 
@@ -80,6 +119,13 @@ class Client:
         )
 
     def start_stream(self, fs_config, fb_config: FbPb.StreamControlStart):
+        """
+        Start streaming data from io4edge function block.
+        @param fs_config: protobuf message with the function specific configuration
+        @param fb_config: protobuf message with the function block configuration
+        @raises RuntimeError: if the command fails
+        @raises TimeoutError: if the command times out
+        """
         fs_any = AnyPb.Any()
         fs_any.Pack(fs_config)
 
@@ -90,12 +136,23 @@ class Client:
         self._command(fb_cmd)
 
     def stop_stream(self):
+        """
+        Stop streaming data from io4edge function block.
+        @raises RuntimeError: if the command fails
+        @raises TimeoutError: if the command times out
+        """
         fb_cmd = FbPb.Command()
         stop = FbPb.StreamControlStop()
         fb_cmd.streamControl.stop.CopyFrom(stop)
         self._command(fb_cmd)
 
     def read_stream(self, timeout, stream_data):
+        """
+        Read next message from stream.
+        @param timeout: timeout in seconds
+        @param stream_data: protobuf message that is filled with the stream data
+        @return functionblock stream meta data (deliveryTimestampUs, sequence)
+        """
         self._stream_queue_sema.acquire(timeout=timeout)
         with self._stream_queue_mutex:
             data = self._stream_queue.popleft()
@@ -108,7 +165,7 @@ class Client:
             self._cmd_event.clear()
             self._client.write_msg(cmd)
             if not self._cmd_event.wait(timeout=self._cmd_timeout):
-                raise RuntimeError("Command timed out")
+                raise TimeoutError("Command timed out")
 
             response = self._cmd_response
             if response.context.value != str(self._cmd_context):
@@ -144,6 +201,10 @@ class Client:
         self._stream_queue_sema.release()
 
     def close(self):
+        """
+        Close the connection to the function block, terminate read thread.
+        After calling this method, the object is no longer usable.
+        """
         self._read_thread_stop = True
         self._read_thread_id.join()
         self._client.close()
