549413ae5afbaa7d9cbf4612ac3667b5a8d2cd2f
popples <klauspopp@gmx.de>
2025-04-10 12:47:53 +0000

    impl bitbussniffer

---
+++ examples/bitbussniffer/dumpstream.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# SPDX-License-Identifer: Apache-2.0
+import io4edge_client.bitbussniffer as bbsniffer
+import io4edge_client.functionblock as fb
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(description="dump stream from bitbus sniffer")
+    parser.add_argument(
+        "addr", help="MDNS address or IP:Port of the bbsniffer function block", type=str
+    )
+    parser.add_argument(
+        "--lowlatency",
+        help="Use stream low latency mode",
+        action="store_true",
+    )
+    args = parser.parse_args()
+
+    bb_client = bbsniffer.Client(args.addr)
+
+    bb_client.start_stream(
+        fb.Pb.StreamControlStart(
+            bucketSamples=100,
+            keepaliveInterval=1000,
+            bufferedSamples=200,
+            low_latency_mode=args.lowlatency,
+        ),
+    )
+
+    while True:
+        try:
+            generic_stream_data, stream_data = bb_client.read_stream(timeout=3)
+        except TimeoutError:
+            print("Timeout while reading stream")
+            continue
+
+        print(
+            "Received %d samples, seq=%d"
+            % (len(stream_data.samples), generic_stream_data.sequence)
+        )
+
+        for sample in stream_data.samples:
+            print(sample_to_str(sample))
+
+
+def sample_to_str(sample):
+    ret_val = "%10d us: " % sample.timestamp
+    ret_val += f"ADDR: {sample.bitbus_frame[0]:02X} "
+    ret_val += f"CTRL: {sample.bitbus_frame[1]:02X} "
+    for i in range(2,len(sample.bitbus_frame)):
+        ret_val += "%02X " % sample.bitbus_frame[i]
+    ret_val += " "
+
+    ret_val += "flags: %02X" % sample.flags
+    return ret_val
+
+
+if __name__ == "__main__":
+    main()
