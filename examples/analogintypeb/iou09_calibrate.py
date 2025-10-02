cab6becc17c4829b2b1451ed9399d9fe26737ea7
popples <klauspopp@gmx.de>
2025-10-02 14:35:38 +0200

    finalize iou09 calib example (#31)

--- examples/analogintypeb/iou09_calibrate.py
+++ examples/analogintypeb/iou09_calibrate.py
@@ -9,7 +9,7 @@ import time
 NUM_CHANNELS = 8
 SAMPLE_RATE = 1000  # Hz
 GAIN_SETTING = 1  # x1
-
+REF_VOLTAGE = 9.994
 
 def sample_for_calibration(ana_client, channel: int) -> float:
     config = ana.Pb.ConfigurationSet()
@@ -29,10 +29,11 @@ def sample_for_calibration(ana_client, channel: int) -> float:
     n_samples = 0
     all_samples = []
     while n_samples < SAMPLE_RATE:
-        generic_stream_data, stream_data = ana_client.read_stream()
+        _, stream_data = ana_client.read_stream(timeout=5)
         n_samples += len(stream_data.samples)
         all_samples.extend([sample.value[0] for sample in stream_data.samples])
     ana_client.stop_stream()
+    time.sleep(1) # wait for stream stop
 
     avg = sum(all_samples) / len(all_samples)
     print(f"Channel {channel+1} average sample value is {avg}")
@@ -49,7 +50,7 @@ def gain_param_name(channel: int) -> str:
 
 def write_calibration(core_client, channel: int, offset: float, gain: float):
     print(
-        f"Writing calibration parameters for channel {channel+1}: offset={offset}, gain={gain}"
+        f"Writing calibration parameters for channel {channel+1}: offset={offset: .6f}, gain={gain: .6f}"
     )
     core_client.set_persistent_parameter(offset_param_name(channel), f"{offset: .6f}")
     core_client.set_persistent_parameter(gain_param_name(channel), f"{gain: .6f}")
@@ -71,6 +72,11 @@ def prompt(msg: str):
     input(msg)
 
 
+def restart_device(core_client, msg: str):
+    print(msg)
+    core_client.restart()
+    time.sleep(10)
+
 def main():
     parser = argparse.ArgumentParser(
         description="Calibrate IOU09 or MIO09 analog input channels"
@@ -79,19 +85,17 @@ def main():
 
     args = parser.parse_args()
 
-    # ana_client, core_client = create_clients(args.addr)
-    # purge_parameters(core_client)
-    # core_client.restart()
-    # print("Purged device parameters. Restarting device to apply changes...")
-    # time.sleep(10)
+    ana_client, core_client = create_clients(args.addr)
+    purge_parameters(core_client)
+    restart_device(core_client, "Purged device parameters. Restarting device to apply changes...")
 
     print("Offset calibration...")
     offset = [0.0] * NUM_CHANNELS
     gain = [1.0] * NUM_CHANNELS
     ana_client, core_client = create_clients(args.addr)
 
-    for channel in range(1, 2):
-        prompt(f"Connect channel {channel+1} to 0.0V and press Enter")
+    prompt(f"Apply 0.0V to all channels and press Enter")
+    for channel in range(0, NUM_CHANNELS):
         raw_value = sample_for_calibration(ana_client, channel)
         if abs(raw_value) > 0.2:
             raise ValueError("measured value too far from 0.0V")
@@ -99,20 +103,30 @@ def main():
         offset[channel] = raw_value
         write_calibration(core_client, channel, offset[channel], gain[channel])
 
-    core_client.restart()
-    time.sleep(10)
+    restart_device(core_client, "Restarting device to apply changes...")
     ana_client, core_client = create_clients(args.addr)
 
-    for channel in range(1, 2):
-        prompt(f"Connect channel {channel+1} to 10.0V and press Enter")
+    prompt(f"Apply {REF_VOLTAGE} V to all channels and press Enter")
+    for channel in range(0, NUM_CHANNELS):
         raw_value = sample_for_calibration(ana_client, channel)
-        if abs(raw_value) < 0.9 or abs(raw_value) > 1.1:
+        if abs(raw_value) < 0.9*(REF_VOLTAGE/10) or abs(raw_value) > 1.1*(REF_VOLTAGE/10):
             raise ValueError(
-                "measured value too far from 10.0V after offset calibration"
+                f"measured value too far from {REF_VOLTAGE} V"
             )
-        gain[channel] = 1.0 / raw_value
+        gain[channel] = REF_VOLTAGE/10 / raw_value
         write_calibration(core_client, channel, offset[channel], gain[channel])
-        core_client.restart()
+    
+
+    restart_device(core_client, "Restarting device to apply changes...")
+    ana_client, core_client = create_clients(args.addr)
+
+    print("Verifying calibration...")
+    for channel in range(0, NUM_CHANNELS):
+        raw_value = sample_for_calibration(ana_client, channel)
+        if abs(raw_value) < 0.995*(REF_VOLTAGE/10) or abs(raw_value) > 1.005*(REF_VOLTAGE/10):
+            raise ValueError(
+                f"measured value too far from {REF_VOLTAGE} V"
+            )
 
 
 if __name__ == "__main__":
