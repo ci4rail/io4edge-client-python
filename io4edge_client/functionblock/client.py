aae817b6fe9f4b65077c29829b3bf0704273f44b
Jan Jansen <jan@ekf.de>
2025-10-09 13:34:26 +0000

    fix: update import path for google.protobuf.any_pb2 to reflect new structure

--- io4edge_client/functionblock/client.py
+++ io4edge_client/functionblock/client.py
@@ -1,12 +1,11 @@
 # SPDX-License-Identifier: Apache-2.0
 import threading
 from collections import deque
-from time import sleep
 from io4edge_client.base import Client as BaseClient
 from io4edge_client.base.connections import ClientConnection, connectable
 from ..util.any import pb_any_unpack
 import io4edge_client.api.io4edge.python.functionblock.v1alpha1.io4edge_functionblock_pb2 as FbPb
-import google.protobuf.any_pb2 as AnyPb
+import io4edge_client.api.google_wellknowntypes.python.google.protobuf.any_pb2 as AnyPb
 
 
 class Client(ClientConnection):
