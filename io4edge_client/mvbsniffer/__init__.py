bb1b9e8e03535311be86ea54018f07f10536f257
popples <klauspopp@gmx.de>
2025-10-13 06:30:51 +0000

    fix mvbsniffer example

--- io4edge_client/mvbsniffer/__init__.py
+++ io4edge_client/mvbsniffer/__init__.py
@@ -2,5 +2,6 @@
 
 from .client import Client
 import io4edge_client.api.mvbSniffer.python.mvbSniffer.v1.mvbSniffer_pb2 as Pb
+import io4edge_client.api.mvbSniffer.python.mvbSniffer.v1.telegram_pb2 as TelegramPb
 
-__all__ = ["Client", "Pb"]
+__all__ = ["Client", "Pb", "TelegramPb"]
