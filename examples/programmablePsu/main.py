3224617b690a97684f46b6a38e07bdcfc13ecbf9
popples <klauspopp@gmx.de>
2025-10-20 09:37:37 +0200

    xq2 example update

--- examples/programmablePsu/main.py
+++ examples/programmablePsu/main.py
@@ -16,13 +16,22 @@ def main():
 
     psu_client = psu.Client(args.addr)
 
-    psu_client.set_voltage_level(5.0)
-    psu_client.set_current_limit(1.0)
+    psu_client.set_voltage_level(10)
+    psu_client.set_current_limit(0.3)
     psu_client.set_output_enabled(True)
 
     while True:
         state = psu_client.get_state()
-        print(f"State: {state}")
+        print(f"           Output State: {psu.Pb.FunctionControlGetResponse.OutputState.Name(state.output_state)}")
+        print(f"             Diag Flags: {state.diag_flags}")
+        print(f"        Desired Voltage: {state.desired_voltage: .3f} V")
+        print(f"Measured Output Voltage: {state.measured_output_voltage: .3f} V")
+        print(f" Measured Sense Voltage: {state.measured_sense_voltage: .3f} V")
+        print(f"      Set Current Limit: {state.current_limit: .3f} A")
+        print(f"       Measured Current: {state.measured_current: .3f} A")
+        print(f"            Temperature: {state.temperature: .1f} °C")
+        print("--------------------------------")
+        
         time.sleep(1)
 
 
